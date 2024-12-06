ac1531ce1dd34745e05870008d54c07dadeba285
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-12-06 13:13:52 +0900

    変数宣言

--- section3/various.py
+++ section3/various.py
@@ -1,5 +1,17 @@
-num = 1
+# num = 1
 name = 'Mike'
+is_ok = True
+num = name
+name= "1"
 
-print(num)
-print(name)
+# 型を推測できる
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+# strになる
+print(num, type(num))
+
+# 型変換も可能である
+new_num = int(name)
+print(new_num, type(new_num))
