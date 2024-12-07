524f59b7b333118b67ec5a37f44587dc019ce67f
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-12-07 21:49:22 +0900

    str

---
+++ section3/4_str.py
@@ -0,0 +1,26 @@
+print("test")
+print("hello")
+print("hello.\nHow are you?")
+
+print(r"C: \name\name")
+
+print("########")
+print(
+    """\
+line1
+line2
+line3\
+"""
+)
+
+print("########")
+
+print("Hi." * 3 + "Mike")
+
+print("Py" "thon")
+prefix = "Py"
+print(prefix + "thon")
+
+s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa" "bbbbbbbbbbbbbbbb"
+
+print(s)
